bf2bb8117f022dabf132b64a60b01f96944d2aeb
kaamessh <kaamesh7120006@gmail.com>
2025-12-07 17:05:23 +0530

    Revert "co"
    This reverts commit 96d35e2914b6ece417070b0d48a3759c29c423d2.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/responsive.css';
+import './theme.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
-
-// Register service worker (PWA)
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js')
-			.then((reg) => console.log('Service worker registered', reg.scope))
-			.catch((err) => console.warn('Service worker registration failed', err));
-	});
-}
